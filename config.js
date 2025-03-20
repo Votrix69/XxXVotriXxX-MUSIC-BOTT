f2ad03fb41dab365738e9d5a604002df62f3353b
Votrix69 <162795742+Votrix69@users.noreply.github.com>
2025-03-20 18:09:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,28 @@
-{
-  "prefix": "!",
-  "token": "https://api.render.com/deploy/srv-cvdhhslds78s73b6bps0?key=smHsMLwQmog"
+
+
+
+module.exports = {
+  TOKEN: "",
+  language: "en",
+  ownerID: ["1004206704994566164", ""], 
+  mongodbUri : "",
+  spotifyClientId : "",
+  spotifyClientSecret : "",
+  setupFilePath: './commands/setup.json',
+  commandsDir: './commands',  
+  embedColor: "#1db954",
+  activityName: "YouTube Music", 
+  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  embedTimeout: 5, 
+  errorLog: "", 
+  nodes: [
+     {
+      name: "GlaceYT",
+      password: "glaceyt",
+      host: "193.226.78.187",
+      port:  9372,
+      secure: false
+    }
+  ]
 }
