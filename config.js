e25a6d13e7b08a97600197276f39cfdf0d0ce167
Votrix69 <162795742+Votrix69@users.noreply.github.com>
2025-03-20 17:59:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,27 +1,4 @@
-
-
-module.exports = {
-  TOKEN: "",
-  language: "en",
-  ownerID: ["630827684037132309", ""], 
-  mongodbUri : "mongodb+srv://atlas-sample-dataset-load-67db16a67e93545ac1f8e058:<db_password>@cluster0.ek7o2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
-  setupFilePath: './commands/setup.json',
-  commandsDir: './commands',  
-  embedColor: "#1db954",
-  activityName: "YouTube Music", 
-  activityType: "https://www.twitch.tv/votrixx__",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
-  embedTimeout: 5, 
-  errorLog: "", 
-  nodes: [
-     {
-      name: "GlaceYT",
-      password: "glaceyt",
-      host: "193.226.78.187",
-      port:  9372,
-      secure: false
-    }
-  ]
+{
+  "prefix": "!",
+  "token": "https://api.render.com/deploy/srv-cvdhhslds78s73b6bps0?key=smHsMLwQmog"
 }
